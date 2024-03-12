b28109f616521fd83c8f2f110bbdd06c077f5baa
Hiyarunss <144218408+heeyarun04@users.noreply.github.com>
2024-03-12 13:56:19 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from sklearn.preprocessing import MinMaxScaler
 
 ##########################
 sns.set(style='dark')
